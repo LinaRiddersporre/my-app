7e00668224f5b7694fbebd37760e6b9cdf945f73
Lina <linariddersporre@gmail.com>
2023-05-07 13:28:39 +0200

    create a ground

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ const App = () => {
   console.log('hej')
   return (
     <div className="App">
-      
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">Projects</a>
+        <a href="#">Contact</a>
+      </nav>
+      <div className='pitchFrace'>
+        <h1>Lina Riddersporre</h1>
+      </div>
     </div>
   );
 }
