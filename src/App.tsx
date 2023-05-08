d97a145d67d14cf173addc32d63473a625b47236
Lina <linariddersporre@gmail.com>
2023-05-08 19:43:45 +0200

    a drop down start

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,50 @@
 import './App.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
 
 const App = () => {
-  console.log('hej')
+const options = [
+  'one', 'two', 'three'
+];
+const defaultOption = options[0];
+  let isOpened = false;
+  const menuClick = (event: any) => {
+    event.preventDefault();
+    console.log(isOpened)
+   isOpened=true
+   console.log(isOpened)
+  }
+
   return (
     <div className="App">
-      <nav>
-        <a href="#">Home</a>
-        <a href="#">Projects</a>
-        <a href="#">Contact</a>
+      <nav className='navMobile'>
+        <MenuIcon className='menuIcon' onClick={menuClick}/>
+        <Dropdown options={options}  value={defaultOption} placeholder="Select an option" />;
+        <div className='dropdownContent' style={isOpened ? {display: 'block'} : {display: 'none'}}>
+          <a href="#">Home</a>
+          <a href="#">Projects</a>
+          <a href="#">Contact</a>
+        </div>
       </nav>
-      <div className='pitchFrace'>
-        <h1>Lina Riddersporre</h1>
+        <nav className='navDesktop'>
+            <a href="#">Home</a>
+            <a href="#">Projects</a>
+            <a href="#">Contact</a>
+          </nav>
+      <span className='firstPage'>
+        <div className='pitchFrace'>
+            <h1>Hi, I'm Lina </h1>
+            <h1>Front end developer</h1>
+          </div>
+      </span>
+      <span>
+        <div>
+        <h1>Project</h1>
       </div>
+      </span>
+      
     </div>
   );
 }
