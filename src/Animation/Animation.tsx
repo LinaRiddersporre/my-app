15843c74748f1cd94fc92329eeb2f2e742632e9f
Lina <linariddersporre@gmail.com>
2023-08-19 16:52:50 +0200

    stylechanges

--- src/Animation/Animation.tsx
+++ src/Animation/Animation.tsx
@@ -17,17 +17,20 @@ export const MyComponent = () => {
                     gyroControls: false,
                     scale: 1.0,
                     scaleMobile: 1.0,
-                    color: 0xde18de,
                     backgroundColor: '#5F5C6D',
                     points: 14.00,
+                    maxDistance: 25.00,
+                    spacing: 17.00,
+                    shininess: 19.00,
+                    color: 'hotpink',
                 })
             );
         }
     }, [vantaEffect]);
     return (
-        <div ref={vantaRef}>
+        <div ref={vantaRef} >
             <p>
-                <h1>Hi, I'm <h2 style={{color: 'black'}}>Lina</h2> </h1>
+                <h1>Hi, I'm <h2 style={{ color: 'hotpink' }}>Lina</h2> </h1>
                 <h1>Front end developer</h1>
             </p>
         </div>
