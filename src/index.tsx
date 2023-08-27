ee33f18598d52a577cd5220668270de54252decf
Lina <linariddersporre@gmail.com>
2023-08-27 08:47:52 +0200

    update route

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={window.location.pathname || ''}>
+    <BrowserRouter>
       <Routes>
         <Route path='/my-app' element={<App />} />
         <Route path='/my-app/adessoProject' element={<AdessoProject />} />
